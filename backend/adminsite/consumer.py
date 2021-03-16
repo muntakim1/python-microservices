dc1f907bb013b2ddd0345bf62879a59171514aa1
Muntakimur Rahaman <32785930+muntakim1@users.noreply.github.com>
2021-03-16 13:41:11 +0600

    Update consumer.py

--- backend/adminsite/consumer.py
+++ backend/adminsite/consumer.py
@@ -2,7 +2,7 @@
 import pika
 
 params = pika.URLParameters(
-    "amqps://jskrsdpc:Vv1dDx6RaCDJIU7KyF4qVtBAHqD6HQ8X@toad.rmq.cloudamqp.com/jskrsdpc")
+    "your-cloudmq-address")
 
 connection = pika.BlockingConnection(params)
 channel = connection.channel()
